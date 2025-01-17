47e8c4a13770bf7ac54aad2a9b436c4afb812074
Avinash123Avinash <avinashaditya2000@gmail.com>
2025-01-18 01:44:21 +0530

    Your commit message

---
+++ js/chatbot.js
@@ -0,0 +1,8 @@
+function openDialog() {
+    document.getElementById('dialog').style.display = 'flex';
+}
+
+// Function to close the dialog
+function closeDialog() {
+    document.getElementById('dialog').style.display = 'none';
+}
